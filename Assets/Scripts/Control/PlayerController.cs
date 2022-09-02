5d50c58bf81d534de813c7baf06e7f7bde8864b1
suprcream <suprcream.gg@gmail.com>
2022-09-02 10:15:16 +0700

    Adding Enemy Prefabs and more

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using RPG.Movement;
+
+namespace RPG.Control {
+    public class PlayerController : MonoBehaviour{
+        private void Update() {
+            if(Input.GetMouseButton(0)){
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor(){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if(hasHit){
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+    }
+}
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -2,29 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-public class Mover : MonoBehaviour
+
+namespace RPG.Movement
 {
+    public class Mover : MonoBehaviour {
     [SerializeField] Transform target;
     // ^^^^^^^^^^^    =    private & deklarasi variable dan nanti bisa muncul di unity editor
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            MoveToCursor();
-        }
         UpdateAnimator();
     }
-    private void MoveToCursor(){
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if(hasHit){
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
-    }
+
+    public void MoveTo(Vector3 destination){
+        GetComponent<NavMeshAgent>().destination = destination;
+    }    
+    
     private void UpdateAnimator(){
         Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
         Vector3 localVelocity = transform.InverseTransformDirection(velocity);
         float speed = localVelocity.z;
         GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
+}
 }
